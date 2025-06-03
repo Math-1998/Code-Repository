85b06ad3f32c7b2d1379063a3615f201594a1a77
Math <matheussoares@wsidm.com.br>
2025-06-03 18:23:16 -0300

    Ajustes no tema

--- Temas/wordpress/wp-content/themes/wsi-blog/functions.php
+++ Temas/wordpress/wp-content/themes/wsi-blog/functions.php
@@ -3,7 +3,6 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-
 /**
  * Constants
  */
@@ -20,21 +19,15 @@ add_action('wp_enqueue_scripts', 'wsi_blog_enqueue');
 function wsi_blog_enqueue()
 {
 
-    /* Enqueue styles */
-
-    $style_list = [
-        'normalize' => 'normalize.css',
-        'main' => 'main.css',
-        'tailwind-custom' => 'tailwind.css',
-    ];
-    foreach ($style_list as $handler => $file) {
-        wp_enqueue_style($handler, CSS_PATH . $file, [], '1.0.0', 'all');
-    };
-
     /* Enqueue Core CSS */
 
-    wp_enqueue_style( 'wsi-core', CSS_PATH . 'app.css', [], filemtime(THEME_PATH . '/assets/app.css'), 'all' );
+    wp_enqueue_style( 'core-style', CSS_PATH . 'output.css', [], filemtime(THEME_PATH . '/assets/app.css'), 'all' );
+    wp_enqueue_style( 'swiper-style', 'https://cdn.jsdelivr.net/npm/swiper@11.2.8/swiper-bundle.min.css', [], '11.2.8', 'all' );
 
     /* Enqueue scripts */
+
+    wp_enqueue_script( 'swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11.2.8/swiper-bundle.min.js', [], '11.2.8', [
+        'strategy' => 'defer', 'in_footer' => true
+    ] );
 
 };
